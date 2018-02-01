77b0277376ba415b8e78c584674da0cee383cc67
Christoph Brandstaetter <CB@christophs-mbp.home>
2018-02-01 17:05:06 +0100

    mongoose connect change to let db...

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -6,13 +6,13 @@ mongoose.Promise = global.Promise;
 
 //mongodb://<dbuser>:<dbpassword>@ds121575.mlab.com:21575/todo-app-test
 
-// let db = {
-//   localhost: 'mongodb://localhost:27017/TodoApp',
-//   mlab: 'mongodb://christoph:NmW-C3D-ZMF-6Lm@ds121575.mlab.com:21575/todo-app-test'
-// };
-//mongoose.connect( db.localhost || db.mlab);
+let db = {
+  localhost: 'mongodb://localhost:27017/TodoApp',
+  mlab: 'mongodb://christoph:NmW-C3D-ZMF-6Lm@ds121575.mlab.com:21575/todo-app-test'
+};
+mongoose.connect( db.localhost || db.mlab);
 
-mongoose.connect(process.env.MONGODB_URI ||'mongodb://localhost:27017/TodoApp');
+//mongoose.connect(process.env.MONGODB_URI ||'mongodb://localhost:27017/TodoApp');
 
 
 module.exports = {mongoose};
