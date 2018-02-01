9c18d3013b83259b048ccd25b53d1dcd5988f257
Christoph Brandstaetter <CB@christophs-mbp.home>
2018-02-01 16:59:11 +0100

    setup for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,18 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
 
+//mongoose.connect('mongodb://localhost:27017/TodoApp');
+
+//mongodb://<dbuser>:<dbpassword>@ds121575.mlab.com:21575/todo-app-test
+
+// let db = {
+//   localhost: 'mongodb://localhost:27017/TodoApp',
+//   mlab: 'mongodb://christoph:NmW-C3D-ZMF-6Lm@ds121575.mlab.com:21575/todo-app-test'
+// };
+//mongoose.connect( db.localhost || db.mlab);
+
+mongoose.connect(process.env.MONGODB_URI ||'mongodb://localhost:27017/TodoApp');
 
 
 module.exports = {mongoose};
--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
 
 var app = express();
+var port = process.env.PORT || 3000;
 
 
 app.use(bodyParser.json());
@@ -64,8 +65,8 @@ app.get('/todos/:id', (req, res) => {
 
 
 
-app.listen(3000, () => {
-    console.log('started on port 3000')
+app.listen(port, () => {
+    console.log(`started on port ${port}`)
 });
 
 
